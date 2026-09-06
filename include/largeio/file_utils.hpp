b9a0fa24b7af6fb6da501246ab45da730e9cd510
boyjayy <popkov.ad@icloud.com>
2026-09-07 00:20:28 +0300

    fd raii full

--- include/largeio/file_utils.hpp
+++ include/largeio/file_utils.hpp
@@ -18,6 +18,20 @@ namespace largeio {
             }
         }
         int get() const noexcept { return _fd; }
+        // запрет копирование тк fd не может быть скопирован только перемещен (a.fd = 3 b.fd = 3 нереален ) 
+        FileDescriptor(const FileDescriptor&) = delete;
+        FileDescriptor& operator=(const FileDescriptor&) = delete;
+        // разрешаем перемещение
+        FileDescriptor(FileDescriptor&& other) noexcept : _fd(std::exchange(other._fd, -1)) {}
+        FileDescriptor& operator=(FileDescriptor&& other) noexcept {
+            if (this != &other) {
+                if (_fd != -1) {
+                    ::close(_fd);
+                }
+                _fd = std::exchange(other._fd, -1);
+            }
+            return *this;
+        }
     };
 
     bool is_regular_file(const fs::path& path);
